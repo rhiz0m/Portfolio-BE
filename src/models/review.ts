d2e929ea21c83d888f904029284342b4492e2796
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-08-27 11:13:36 +0200

    Add - review interface. Add - ReviewService

---
+++ src/models/review.ts
@@ -0,0 +1,9 @@
+
+export interface Review {
+    id: string,
+    title: String,
+    date: Date,
+    comment: String
+    author: String
+}
+
---
+++ src/services/reviewService.ts
@@ -0,0 +1,12 @@
+import { v4 as uudiv4 } from "uuid" 
+import { Review } from  "../models/review"
+
+export const createReview = (title: string, comment: string, author: string): Review => {
+    return {
+        id: uudiv4(),
+        title,
+        date: new Date(),
+        comment,
+        author
+    }
+}
